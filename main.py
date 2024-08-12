c873751f6f25bf5e6d4c75fd48f382c6ac03041a
jdavies08 <jackdavies08@icloud.com>
2024-08-12 12:04:32 +0100

    face detection
    code is now able to detect faces and draw a box around them

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import cv2
+import cv2, face_recognition
 #get webcam
 cap = cv2.VideoCapture(0)
 while True:
 	q , frame = cap.read()
+	#detect the faces
+	for top,right,bottom,left in face_recognition.face_locations(frame):
+		
+		#draw rectangle around faces
+		cv2.rectangle(frame,(left,top),(right,bottom),(0,255,0), 5) 
 	
+	
+	
+	
+	#shows the image
 	cv2.imshow("window",frame)
+	#quit if q is pressed
 	if cv2.waitKey(1) == ord("q"):
 		break
 
