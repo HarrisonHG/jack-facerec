b9206c0c06598cfc9489aeed6e1734654c4820cc
jdavies08 <jackdavies08@icloud.com>
2024-08-13 15:59:31 +0100

    fixed headshots.py creating folders and clarifed variables

--- headshots.py
+++ headshots.py
@@ -1,18 +1,20 @@
 import cv2
 import os
+import functions
 
 DATASET_FOLDER = "dataset"
 NAME = 'donkeykong' #replace with your name
+CAMINDEX = 0
 
-cam = cv2.VideoCapture(0)
+cam = functions.camera_connection(CAMINDEX)
 
 cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("press space to take a photo", 500, 300)
 
-if no os.path_exists(DATASET_FOLDER):
+if not os.path.exists(DATASET_FOLDER):
     os.makedirs(DATASET_FOLDER)
     
-if no os.path_exists(NAME):
+if not os.path.exists(NAME):
     os.makedirs(NAME)
 
 img_counter = 0
@@ -31,11 +33,9 @@ while True:
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+        img_name = "DATASET_FOLDER/"+ name +"/image_{}.jpg".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
-
-cam.release()
 
 cv2.destroyAllWindows()
