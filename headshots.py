d9c31893be0b214cb79772a369790573b96e7294
jdavies08 <jackdavies08@icloud.com>
2024-08-14 09:04:59 +0100

    Fixed error with directory not being made correctly

--- headshots.py
+++ headshots.py
@@ -8,14 +8,16 @@ CAMINDEX = 0
 
 cam = functions.camera_connection(CAMINDEX)
 
-cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("press space to take a photo", 500, 300)
-
 if not os.path.exists(DATASET_FOLDER):
     os.makedirs(DATASET_FOLDER)
     
-if not os.path.exists(NAME):
-    os.makedirs(NAME)
+if not os.path.exists(DATASET_FOLDER+"/"+NAME):
+    os.makedirs(DATASET_FOLDER +"/"+ NAME)
+
+cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("press space to take a photo", 500, 300)
+
+
 
 img_counter = 0
 
@@ -33,7 +35,7 @@ while True:
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "DATASET_FOLDER/"+ name +"/image_{}.jpg".format(img_counter)
+        img_name = DATASET_FOLDER+"/"+ NAME +"/image_{}.jpg".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
